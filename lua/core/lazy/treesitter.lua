f2962ff61c39771de19c70e8f38b42fedceef681
Myles Rademaker <mylesprademaker@gmail.com>
2025-12-05 15:49:14 -0800

    Added treesitter defaults for gdscript and c# for godot projects

--- lua/core/lazy/treesitter.lua
+++ lua/core/lazy/treesitter.lua
@@ -6,7 +6,21 @@ return {
         build = ':TSUpdate',
         config = function()
             require('nvim-treesitter.configs').setup {
-                ensure_installed = { 'javascript', 'typescript', 'c', 'lua', 'vim', 'vimdoc', 'query', 'markdown', 'markdown_inline' },
+                ensure_installed = {
+                    'javascript',
+                    'typescript',
+                    'gdscript',
+                    'godot_resource',
+                    'gdshader',
+                    'c_sharp',
+                    'c',
+                    'lua',
+                    'vim',
+                    'vimdoc',
+                    'query',
+                    'markdown',
+                    'markdown_inline',
+                },
                 auto_install = false,
                 highlight = {
                     enable = true,
